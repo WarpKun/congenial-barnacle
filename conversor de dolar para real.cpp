89678a4c41ae41d3a40c5eb8266698bdeb69d2cc
WarpKun <141471713+WarpKun@users.noreply.github.com>
2023-08-19 14:38:34 -0300

    Add files via upload
    colocando algumas coisas a mais, como uma saida para o menu, ele nao fechar automaticamente depois de converter um valor, mexendo um pouco na estetica ao rodar o programa e organizando melhor o codigo

--- conversor de dolar para real.cpp
+++ conversor de dolar para real.cpp
@@ -1,47 +1,68 @@
 #include <cstdio>
 #include <cstdlib> 
+#include <locale>
 
 int main()
 {		
+	setlocale(LC_ALL, "Portuguese");
+	
 	double dolar = 5.20;
 	double real = 1.0 / dolar;
 	
 	int opcao;
 	double valor;
 	
-	printf("escolha a opcao desejada:\n");
-	printf("1- converter real para dolar\n");
-	printf("2- converter dolar para real\n");
-	scanf("%d", &opcao);
-
-	
-		switch (opcao)
-		{
-			case 1:
-				
-				printf("coloque o valor em reias:");
-				scanf("%lf", &valor );
-				printf("Valor em dolares: %.2f\n", valor / dolar);
-				break;
-			
-			
-			case 2:
-			
-				printf("coloque o valor em dolar");
-				scanf("%lf", &valor);
-				printf("valor em real: %.2f", valor * dolar);
-				break;
-				
+	do{
+			printf("       Bem vindo ao conversor!\n\n\n");
+			printf("O que deseja fazer hoje?\n\n");
+			printf("1-converter real para dolar\n");
+			printf("2-converter dolar para real\n\n");
+			printf("0-Sair\n\n");
+			printf("Resposta:");
+			scanf("%d", &opcao);
+			system("cls");
 		
-			default:
 			
-				printf("opcao invalida");
-				break;
+				switch (opcao)
+				{
+					
+					case 0:
+						
+						printf("obrigado por usar o nosso programa\n\n");
+						break;
+			
+					
+					case 1:
+						
+						printf("coloque o valor em reias: ");
+						scanf("%lf", &valor );
+						printf("Valor em dolares: %.2f\n\n", valor / dolar);
+						system("pause");
+						system("cls");
+						break;
+					
+					
+					case 2:
+					
+						printf("coloque o valor em dolar: ");
+						scanf("%lf", &valor);
+						printf("valor em real: %.2f\n\n", valor * dolar);
+						system("pause");
+						system("cls");
+						break;
+						
+				
+					default:
+					
+						printf("opçao invalida\n\n");
+						system("pause");
+						system("cls");
+						break;
 			
 		
-		}
-	
-	system("pause");
+				}
+		}while (opcao !=0);
+		
 	return 0;
 	
 	
